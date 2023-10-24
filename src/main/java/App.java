6e0a14d91344a7ecad4eff3f79b4a917f193b3d2
HienDinh <dnmhien3010@gmail.com>
2023-10-24 15:15:21 -0500

    Test Trigger in Dev Branch

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello git hub action");
         System.out.println("Learn trigger action");
+        System.out.println("dev branch test to not trigger even");
     }
 }
