e09a1baff19a4afba60acf50c1133b9879afd883
HienDinh <dnmhien3010@gmail.com>
2023-10-24 15:28:52 -0500

    Handle pull request

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,5 +3,6 @@ public class App {
         System.out.println("Hello git hub action");
         System.out.println("Learn trigger action");
         System.out.println("workflow_dispatch");
+        System.out.println("Handle pull request");
     }
 }
